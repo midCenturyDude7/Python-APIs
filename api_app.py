e33c737e4a9c540ed9a30a797da364b20484589e
midCenturyDude7 <mgriffes@gmail.com>
2020-06-23 21:39:17 -0700

    First commit

---
+++ api_app.py
@@ -0,0 +1,18 @@
+# Acknowledgements: "All You Need To Know on APIs With Python" - James Briggs
+# Medium Blog Post: https://towardsdatascience.com/all-you-need-to-know-on-apis-with-python-927fb572d723
+
+"""
+File: api_app.py
+Practice with APIs using Python
+"""
+
+import requests
+
+
+def main():
+
+    pass
+
+
+if __name__ == '__main':
+    main()
